91c7b85835301467c13c6f6aea04bec3ae5b050c
Adeleye Olayinka <adeleyeolayinka73@gmail.com>
2023-11-21 19:40:58 +0100

    feat: Add random password generator (#1)
    ### This PR adds a random password generator webpage
    link to figma design:
    [@figma-design](https://www.figma.com/file/YRO9Iw5IYaOorjnRyNz4bV/Random-Password-Generator?type=design&node-id=102-702&mode=design&t=niQgxhqbdENYHX2U-0)

---
+++ script.js
@@ -0,0 +1,57 @@
+const passwordCharacters =
+  '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&.*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const passwordInputs = document.querySelectorAll('.password-inputs');
+
+const generateRandomPassword = () => {
+  // This code block generates any random password
+  const passwordLength = 10;
+  let password = '';
+  for (let i = 0; i <= passwordLength; i += 1) {
+    const randomNumber = Math.floor(Math.random() * passwordCharacters.length);
+    password += passwordCharacters.charAt(randomNumber);
+  }
+  return password;
+};
+
+const forEachPasswords = () => {
+  // This code block assigns unique passwords for each input on its own
+  for (let i = 0; i < passwordInputs.length; i += 1) {
+    const inputs = passwordInputs[i];
+    inputs.value = generateRandomPassword();
+  }
+};
+
+const copyPasswordOne = () => {
+  const text = document.querySelector('#input-1').value;
+  navigator.clipboard.writeText(text);
+};
+
+const copyPasswordTwo = () => {
+  const text = document.querySelector('#input-2').value;
+  navigator.clipboard.writeText(text);
+};
+
+const copyPasswordThree = () => {
+  const text = document.querySelector('#input-3').value;
+  navigator.clipboard.writeText(text);
+};
+
+const copyPasswordFour = () => {
+  const text = document.querySelector('#input-4').value;
+  navigator.clipboard.writeText(text);
+};
+
+const copyFirstPassword = document.querySelector('#copy-1');
+copyFirstPassword.addEventListener('click', copyPasswordOne);
+
+const copySecondPassword = document.querySelector('#copy-2');
+copySecondPassword.addEventListener('click', copyPasswordTwo);
+
+const copyThirdPassword = document.querySelector('#copy-3');
+copyThirdPassword.addEventListener('click', copyPasswordThree);
+
+const copyForthPassword = document.querySelector('#copy-4');
+copyForthPassword.addEventListener('click', copyPasswordFour);
+
+const generateButton = document.querySelector('#generate-btn');
+generateButton.addEventListener('click', forEachPasswords);
